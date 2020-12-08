Minor changes, added function to generate masks from OUFTI, fixed bug in confusion matrix calculation in compute pixel stats

mrcnn/mrcnn_inspect.py
@@ -124,7 +124,7 @@ def compute_pixel_metrics(dataset, image_ids):  # Custom function.
         for unique_class_id in np.unique(gt_class_id):
             idx = [i for i, cls in enumerate(gt_class_id) if cls == unique_class_id]  # Find matching indicies
             gt_masks_perclass = gt_mask[:,:,idx]  # extract masks per class
-            pred_masks_perclass = results[0]['masks'][:,:,idx]
+            pred_masks_perclass = results[0]['masks']
 
             assert ((gt_masks_perclass == 0) | (gt_masks_perclass == 1)).all()  # Assert masks are strictly binary
             assert ((pred_masks_perclass == 0) | (pred_masks_perclass == 1)).all()
@@ -216,21 +216,23 @@ if __name__ == '__main__':
     tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)  # Log errors only.
 
     DEVICE = "/gpu:0"  # /cpu:0 or /gpu:0
-    WEIGHTSDIR = os.path.join(get_parent_path(0),'trial_rotate+translate+defocus+noise+cutout_lr=0.003_bs=2_t=20201029T1746', 'mask_rcnn_trial_rotate+translate+defocus+noise+cutout_lr=0.003_bs=2_t=.h5') # Foldername with weights. Prediction will run with latest weights.
+    WEIGHTSDIR = os.path.join(get_parent_path(0),'trial1_equalized_channels_dataset1lr=0.003_bs=2_t=20201205T1841', 'mask_rcnn_trial1_equalized_channels_dataset1lr=0.003_bs=2_t=.h5') # Foldername with weights. Prediction will run with latest weights.
 
     # create config
     config = PredictionConfig()  # Import test config
     config.IMAGES_PER_GPU = 1
     config.BATCH_SIZE = 1
     config.display()
+    #config.DETECTION_NMS_THRESHOLD = 0.2
+    #config.POST_NMS_ROIS_INFERENCE = 1000
 
     config_train = BacConfig()  # Import training config
 
     # TODO: Merge training and prediction configs for more robustness
 
     # Data for prediction
-    testdir = os.path.join(get_parent_path(1), 'Data', 'Dataset1_27_10_20', 'Test')
-    traindir = os.path.join(get_parent_path(1), 'Data', 'Dataset1_27_10_20', 'Train')
+    testdir = os.path.join(get_parent_path(1), 'Data', 'Dataset1_05_12_20', 'Test')
+    traindir = os.path.join(get_parent_path(1), 'Data', 'Dataset1_05_12_20', 'Train')
 
 
     dataset = BacDataset()
@@ -403,8 +405,8 @@ if __name__ == '__main__':
         iaa.Sometimes(0.5, iaa.Affine(translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)})),
         # Translate up to 20% on either axis independently, 50% prob
         iaa.Sometimes(0.5, iaa.GaussianBlur(sigma=(0, 2.0))),  # Gaussian convolve 50% prob
-        iaa.Sometimes(0.5, iaa.AdditiveGaussianNoise(scale=(0, 0.05 * 65535))),  # up to 5% PSNR noise 50% prob
-        iaa.Sometimes(0.5, iaa.Cutout(nb_iterations=(1, 10), size=0.05, squared=False, cval=31628.69))
+        #iaa.Sometimes(0.5, iaa.AdditiveGaussianNoise(scale=(0, 0.05 * 65535))),  # up to 5% PSNR noise 50% prob
+        iaa.Sometimes(0.5, iaa.Cutout(nb_iterations=(1, 10), size=0.05, squared=False, cval=0))
     ]
 
     augmentation = iaa.Sequential(seq)  # Execute in sequence from 1st to last
@@ -414,6 +416,7 @@ if __name__ == '__main__':
     for i in range(limit):
         image, image_meta, class_ids, bbox, mask = modellib.load_image_gt(
             dataset_train, config_train, image_id, use_mini_mask=False, augment=False, augmentation=augmentation)
+        image[image == 0] = 25000
         visualize.display_instances(skimage.img_as_ubyte(image), bbox, mask, class_ids,
                                     dataset.class_names, ax=ax[i // 2, i % 2],
                                     show_mask=False, show_bbox=False, title='Augmentation example {}'.format(i))

pipeline/mask_generators.py
@@ -95,17 +95,107 @@ def masks_from_VOTT(**kwargs):
     #At the end, print a summary
 
     sys.stdout.flush()
-    print('\n')
     print('')
-    print('-------------------------------------------')
     print('Generated', ':', str(mask_total), 'masks out of', str(image_total), 'images.')
-    print('-------------------------------------------')
     sys.stdout.flush()
+
+def masks_from_OUFTI(**kwargs):
+
+    '''
+    Locates the OUFTI export .mat files in the mask folder and generates single cell masks in the output folder, in the structure
+    output_folder/annots/(image_name)/Cell1.png... Cell2.png....
+
+    Parameters
+    ----------
+    mask_path : string
+        path to directory with annotations
+    output_path : string
+        path to directory where the output data structure will be created.
+        Function creates a folder called annots. Inside annots, each subdir is a separate image, inside which are binary masks.
+    img_size : 2-tuple
+        (x,y) image size, since metadata is not saved by OUFTI
+    Returns
+    -------
+
+    '''
+
+    mask_path = kwargs.get('mask_path', False)
+    output_path = kwargs.get('output_path', False)
+    image_size = kwargs.get('image_size', False)
+
+    if not all([mask_path, output_path]):  # Verify input
+        raise TypeError
+    import scipy.io, skimage.io, skimage.draw, os, numpy, sys
+    from PIL import Image
+
+    makedir(os.path.join(output_path, 'annots'))  # Create folder with masks
+    image_total = 0
+    mask_total = 0
+    error_count = 0
+    meshless_cell_count = 0
+
+    #Find all annotation files that end with .mat
+    tracker = 0
+    for root, dirs, files in os.walk(mask_path, topdown=True):
+        for file in files:
+            if file.endswith('.mat'):
+
+                img_filename = file.split('.')[0]
+
+                #Load annot file for image
+                loadpath = os.path.join(root,file)
+                annotfile = scipy.io.loadmat(loadpath)
+
+                try:
+                    cellData = annotfile['cellList']['meshData'][0][0][0][0][0] #Load data for all cells in image
+                except:
+                    error_count = error_count + 1
+                    continue
+
+                makedir(os.path.join(output_path, 'annots', img_filename))  # Create folder with masks
+
+                cellcount=1
+                for key,cell in dict(numpy.ndenumerate(cellData)).items(): #Iterate through all cells in image
+
+                    mesh = cell['mesh'] #Get attribute
+
+                    #Attempt to unpack. Skip if empty.
+                    mesh = mesh[0][0]
+                    if mesh.shape == (1,1):
+                        meshless_cell_count = meshless_cell_count +1
+                        continue
+
+                    mesh = numpy.concatenate((mesh[:,:2],mesh[:,2:]),axis=0) #Reslice array to a more conventional format
+                    mesh_tran = numpy.zeros(mesh.shape)
+
+                    x=mesh[:,0]
+                    y=mesh[:,1]
+                    mesh_tran[:,0] = y #Swap columns to match polygon2mask
+                    mesh_tran[:,1] = x
+
+                    mask = skimage.draw.polygon2mask(image_size, mesh_tran)
+
+                    filename = 'Cell' + str(cellcount) + '.bmp'  # Mask filename
+                    savepath = os.path.join(output_path, 'annots', img_filename, filename)  # Assemble whole save path
+
+                    Image.fromarray(mask).save(savepath)
+
+                    mask_total = mask_total + 1
+                    cellcount = cellcount + 1
+                image_total = image_total + 1
+
+    sys.stdout.flush()
+    print('')
+    print('Generated', ':', str(mask_total), 'masks out of', str(image_total), 'images.')
+    print('cellList read errors:', error_count)
+    print('Meshless cells found:', meshless_cell_count )
+    sys.stdout.flush()
+
 
 
 if __name__ == '__main__':
     import os
 
-    input_path = os.path.join(get_parent_path(1),'Data','Phenotype detection_18_08_20','Segregated','Combined','WT+ETOH','Segmentations')
-    output_path = os.path.join(get_parent_path(1), 'Data', 'Phenotype detection_18_08_20', 'Segregated', 'Combined', 'WT+ETOH', 'Segmentations')
-    masks_from_VOTT(input_path, output_path)
+    input_path = os.path.join(r'C:\Users\zagajewski\Desktop\AMR_png_images_for_segmentation\18_08_20\WT_fakePC')
+    output_path = os.path.join(r'C:\Users\zagajewski\Desktop\AMR_png_images_for_segmentation\18_08_20\WT_fakePC')
+    masks_from_OUFTI(mask_path=input_path, output_path=output_path)